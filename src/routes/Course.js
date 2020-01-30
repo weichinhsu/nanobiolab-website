2b507d64d6e39c4b26d89f8fdc6238e85749831c
Winnie Hsu <winnie860403@gmail.com>
2020-01-30 17:06:42 +0800

    set course page

--- src/routes/Course.js
+++ src/routes/Course.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import data from '../data/course'
 
 class Course extends Component {
+  renderRow = (row) => {
+    return <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-2">
+        <div className="resume-content">
+            <div className="subheading mb-1">{row.title}</div>
+            <p>{row.semester}</p>
+        </div>
+
+    </div>
+}
+renderCourse = (course) => {
+    return <div>
+        <h3 className="mb-3">{course.subject}</h3>
+        {course.group.map(row => this.renderRow(row))}
+    </div>
+}
   render() {
     return (
       <section className="resume-section p-3 p-lg-5 d-flex justify-content-center" id="education">
         <div className="w-100">
-          <h2 className="mb-5">Course</h2>
+          <h2 className="mb-5">開授課程</h2>
+          {data.map(course => this.renderCourse(course))}
         </div>
       </section>
     );
