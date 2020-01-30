302657c92c29b8f1be2291b1c11481dfff36cf66
Winnie Hsu <winnie860403@gmail.com>
2020-01-30 17:08:02 +0800

    insert: course data

---
+++ src/data/course.js
@@ -0,0 +1,43 @@
+export default [{
+    "subject": "近期開授課程",
+    "group": [{
+        "title": "2201 電路學 Electric Circuits",
+        "period": "",
+        "semester": "(107上 大學部二年級授課)"
+    }, {
+        "title": "420900 微算機導論 Introduction to Microcomputers",
+        "period": "",
+        "semester": "(98上、100上、101上 、102上、103上、104上、105上、106上、107上大學部四年級授課)"
+    },{
+        "title": "520200 仿生科學與生物混成系統 Biomimetic Science and Biohybrid Systems",
+        "period": "",
+        "semester": "(99下、102下 研究所授課)"
+    },{
+        "title": "550200 生醫感測器與生物電子學 Biosensors and Bioelectronics",
+        "period": "",
+        "semester": "(95下、96下、98下、99下、101下、103下、104下、105下、106下、107下  研究所授課)"
+    },{
+        "title": "523300 生醫微系統 Biomedical Microsystem",
+        "period": "",
+        "semester": "(95上、96上、97下、98上 、99上、100上、101上、102上、103上、104上、105上 、106上、107下研究所授課)"
+    }]
+}, {
+    "subject": "過去開授課程",
+    "group": [{
+        "title": "421700 電控系統與應用 Applications of Electromechanical Control Systems",
+        "period": "",
+        "semester": "(96上、97上、98下、100下 大學部三年級授課)"
+    }, {
+        "title": "321500 生醫微晶片原理與應用 Principles and Applications of Biomedical Lab-on-a-Chips",
+        "period": "",
+        "semester": "(96下、97上、100下 大學部三年級授課)"
+    }, {
+        "title": "200201 工程數學(二) Engineering Mathematics II ",
+        "period": "",
+        "semester": "(100下 大學部二年級授課)"
+    }, {
+        "title": "330400 量測系統與實驗 Measurements Lab",
+        "period": "",
+        "semester": "(95下大學部授課) => 進階改為'電控實驗'課程"
+    }]
+}]
