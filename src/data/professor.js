f91fd9637c9c08f7229905ff4da029333a26adfb
Winnie Hsu <winnie860403@gmail.com>
2020-02-03 23:55:06 +0800

    insert: professor data

---
+++ src/data/professor.js
@@ -0,0 +1,124 @@
+export default [{
+    "subject": "關於",
+    "group": {
+        "name": "Chien-Chong Hong, Ph.D.",
+        "occupation": "Professor, Department of Power Mechanical Engineering",
+        "email": "chong@pme.nthu.edu.tw",
+        "phone": "886-3-5715131 ext. 33736",
+        "address": "101, Sec. 2, Kuang Fu Rd., Hsinchu, Taiwan",
+        "homepage": "www.pme.nthu.edu.tw/nanobiolab",
+        "education": [
+            "Ph. D. in Electrical Engineering - University of Cincinnati, Cincinnati, Ohio, USA, 2004",
+            "M.S. in Electrical Engineering - University of Cincinnati, Cincinnati, Ohio, USA, 2001",
+            "M.S. in Mechanical Engineering - National Taiwan University, Taiwan, 1995",
+            "B.S. in Mechanical Engineering - National Cheng Kung University, Taiwan, 1993"],
+        "experience": [
+            ["Professor, Department of Power Mechanical Engineering, National Tsing Hua Univeristy, Hsinchu, Taiwan", "Aug. 2017 – Present"],
+            ["Associate Professor, Department of Power Mechanical Engineering, National Tsing Hua Univeristy, Hsinchu, Taiwan", "Aug. 2012 – July 2017"],
+            ["Assistant Professor, Department of Power Mechanical Engineering, National Tsing Hua Univeristy, Hsinchu, Taiwan", "Aug. 2006 – July 2012"],
+            ["Adjunct Assistant Professor, Department of Bioengineering, Clemson University, USA", "June 2005 – May 2006"],
+            ["Post-Doctoral Fellow, Proteomics Center, Medical University of South Carolina, USA", "Nov. 2004 – Oct. 2005"],
+        ],
+        "education": [
+            "Biosensors",
+            "Nanopore DNA sequencing, bacteria detection",
+            "Microfluidic biochips, lab on chip, point-of-care diagnostics",
+            "System integration, handheld analyzer, circuit design, I/O interface",
+            "Material inkjet printing, roll-printed siliver nanowire transparent conductive electrodes",
+            "Smart polymers (molecularly imprinted polymers, shape memory polymers, conducting polymers, piezoelectric polymers) and their fundamental study in",
+            "Nanobio interfacing, nanobioelectronics",
+        ],
+    },
+}, {
+    "subject": "期刊論文",
+    "group": [{
+        "title": "Roller-Induced Bundling of Long Silver Nanowire Networks for Strong Interfacial Adhesion, Highly Flexible, Transparent Conductive Electrodes",
+        "author": "Yan-Ren Chen, Chien-Chong Hong*, Tong-Miin Liou*, Kuo Chu Hwang, Tzung-Fang Guo",
+        "period": "2017",
+        "institution": "Scientific Reports",
+    }, {
+        "title": "Handheld Analyzer with Molecularly-Imprinted Biosensors for Electrical Detection of Anesthetic Propofol in Plasma Samples",
+        "author": "Chien-Chong Hong*, Chih-Chung Lin, Chian-Lang Hong*, Zi-Xiang Lin, Meng-Hua Chung, Pei-Wen Hsieh",
+        "period": "201ˊ",
+        "institution": "Biosensors and Bioelectronics",
+    }, {
+        "title": "A Disposable Breath Sensing Tube with On-Tube Single-Nanowire Sensor Array for On-Site Detection of Exhaled Breath Biomarkers",
+        "author": "Chung-Hsuan Wu, Wei-Han Wang, Chien-Chong Hong*, Kuo Chu Hwang",
+        "period": "2016",
+        "institution": "Lab on a Chip",
+    },]
+}, {
+    "subject": "國際會議",
+    "group": [{
+        "title": "On-Chip Plasma Separation with Shape-Memory Vacuum Module for On-Site Rapid Detection from Whole Blood Samples",
+        "author": "Zheng-Lin Wang, Chian-Lang Hong, and Chien-Chong Hong",
+        "period": "May 25-27, 2016",
+        "institution": "Biosensors 2016 (26th Anniversary World Congress on Biosensors), Gothenburg, Sweden",
+    },]
+}, {
+    "subject": "研討會",
+    "group": [{
+        "title": "Interleukin-6 Protein Biosensors Based on Artificially-Made Recognition Elements for Point-of-Care Diagnostics",
+        "author": "Pei-Shan Pai, Ching-Chih Chang, Chien-Chong Hong Tong-Miin Liou, and Chie-Pein Chen",
+        "period": "June 19-21, 2018",
+        "institution": "The 10th International Symposium on Microchemistry and Microsystems (ISMM 2018), Busan, Korea",
+    },]
+}, {
+    "subject": "專利申請",
+    "group": [{
+        "name": "台灣專利",
+        "data": {
+            "title": "生物感測器",
+            "author": "洪健中、吳政穎",
+            "period": "2019/01/18",
+            "id": "108101955",
+        }
+    }, {
+        "name": "美國專利",
+        "data": {
+            "title": "BIOSENSOR",
+            "author": "Chien-Chong Hong, Jheng-Ying Wu",
+            "period": "2019/03/29",
+            "id": "16/370133",
+        },
+    }]
+}, {
+    "subject": "審稿經驗",
+    "group": [{
+        "name": "個人",
+        "data": [
+            "Proposal Funding Reviewer, USA National Science Foundation –IRFP (International Research Fellowship Program)",
+            "Proposal Funding Reviewer, Industrial Technology Research Institute (ITRI), Taiwan",
+            "Proposal Funding Reviewer, Taiwan National Science Council",
+        ]
+    }, {
+        "name": "出版",
+        "data": [
+            "Advanced Materials",
+            "Biosensors & Bioelectronics",
+            "Journal of Microelectromechanical Systems",
+        ]
+    }]
+}, {
+    "subject": "其他",
+    "group": [{
+        "name": "協同活動",
+        "data": [
+            "Associate Editor, Journal of Microelectronic Engineering",
+            "Associate Editor, Micro and Nano Engineering ",
+            "Editorial Board, Journal of Biochips and Tissue Chips",
+            "International Conference Session Chair, 2010 International Conference on Advanced Manufacture",
+            "SEMBA 2012 Session Chair",
+        ]
+    }, {
+        "name": "獲獎紀錄",
+        "data": [
+            "Our sub-20 nm polymer nanotip/nanopore research was selected by Nature Asia Materials for highlighting (2010).",
+            "Our sub-20 nm polymer nanotip/nanopore research was selected by Taiwan National Science Council for highlighting (2010).",
+            "Innovation and Creation Award (NSC Undergraduate Student Research Project), Taiwan National Science Council (2011 )",
+            "Teaching Excellence Award, National Tsing Hua University (2011)",
+            "HIWIN Thesis Award (2013)",
+            "The 11th National Innovation Award in the Academic Research Category by the Institute for Biotechnology and Medicine Industry (2014)",
+        ]
+    }]
+}]
