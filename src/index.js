fd97a2c3c9332dbe931094b43feb4a2610ad2db2
Winnie Hsu <winnie860403@gmail.com>
2020-02-05 00:24:23 +0800

    fix: url basename

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createBrowserHistory as createHistory } from 'history';
 
 // 1. Initialize
 const app = dva({ history: createHistory({ basename: '/nanobiolab-website' }) });
-// const app = dva({ history: createHistory() });
 
 // 2. Plugins
 // app.use({});
