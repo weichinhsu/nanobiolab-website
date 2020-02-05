8ba3902902f4dc7bbee8423d28b562333539b97a
Winnie Hsu <winnie860403@gmail.com>
2020-02-06 00:03:00 +0800

    modify: news and research pages

--- src/routes/News.js
+++ src/routes/News.js
@@ -1,21 +1,59 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
 import animateScrollTo from 'animated-scroll-to';
-
-import news01 from '../assets/images/news/news01.JPG'
-import news02 from '../assets/images/news/news02.jpg'
+import data from '../data/news'
 
 class News extends Component {
-    renderEducation = (education) => {
-        return <div > {education} </div>
+    renderModal = (row) => {
+        return <div className="modal fade" id={row.id} tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+            <div className="modal-dialog modal-dialog-scrollable modal-lg" role="document">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalScrollableTitle">{row.title}</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="col-md-4" >
+                            <img src={"/img/news/" + row.image[0]}
+                                className="card-img img-news"
+                                alt="..." />
+                        </div>
+                        <br />
+                        {row.content.map(cont => <p>{cont}<br /><br /></p>)}
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    }
+    renderNews = (news) => {
+        return <div className="card mb-3" data-toggle="modal" data-target={'#' + news.modal.id}>
+            <div className="row no-gutters" >
+                <div className="col-md-4" >
+                    <img src={"/img/news/" + news.image}
+                        className="card-img img-news"
+                        alt="..." />
+                </div>
+                <div className="col-md-8" >
+                    <div className="card-body" >
+                        <h5 className="card-title" > {news.title} </h5>
+                        {news.content}
+                        {/* <a href="https://udn.com/news/story/7266/4236122" target="_blank">聯合新聞網</a><br />
+                    <a href="https://news.ltn.com.tw/news/life/breakingnews/3014002" target="_blank">自由時報</a><br />
+                    <a href="https://money.udn.com/money/story/12524/4236122" target="_blank">經濟日報</a> */}
+                        <p className="card-text" > < small className="text-muted" > {news.period} </small></p >
+                    </div>
+                </div>
+            </div>
+            {this.renderModal(news.modal)}
+        </div>
     }
     render() {
         return (<div >
-            {
-                /* <div>
-                          <img className="img-fluid mx-auto mb-2" src={background} alt="" />
-                        </div> */
-            }
             <header className="masthead" id="home" >
                 <div className="container" >
                     <div className="intro-text" >
@@ -29,71 +67,10 @@ class News extends Component {
             </header>
             <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="news" >
                 <div className="w-100" >
-                    <h2 > 最新消息 </h2> 
-                    <div className="card mb-3" data-toggle="modal" data-target="#exampleModalScrollable">
-                        <div className="row no-gutters" >
-                            <div className="col-md-4" >
-                                <img src={news01}
-                                    className="card-img img-news"
-                                    alt="..." />
-                            </div>
-                            <div className="col-md-8" >
-                                <div className="card-body" >
-                                    <h5 className="card-title" > 科技部醫材創新計劃舉辦記者會 </h5>
-                                    <a href="https://udn.com/news/story/7266/4236122" target="_blank">聯合新聞網</a><br />
-                                    <a href="https://news.ltn.com.tw/news/life/breakingnews/3014002" target="_blank">自由時報</a><br />
-                                    <a href="https://money.udn.com/money/story/12524/4236122" target="_blank">經濟日報</a>
-                                    <p className="card-text" > < small className="text-muted" > 2019.12.19 </small></p >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card mb-3" >
-                        <div className="row no-gutters" >
-                            <div className="col-md-4" >
-                                <img src={news02}
-                                    className="card-img img-news"
-                                    alt="..." />
-                            </div>
-                            <div className="col-md-8" >
-                                <div className="card-body" >
-                                    <h5 className="card-title" > 本團隊榮獲科技部2019未來科技突破獎 </h5>
-                                    <p className="card-text" > This is a wider card with supporting text below as a natural lead - in to additional content.This content is a little bit longer. </p>
-                                    <p className="card-text" > < small className="text-muted" > 2019.12.05~08 </small></p >
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+                    <h2 > 最新消息 </h2>
+                    {data.map(news => this.renderNews(news))}
                 </div>
-                <div className="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-                    <div className="modal-dialog modal-dialog-scrollable modal-lg" role="document">
-                        <div className="modal-content">
-                            <div className="modal-header">
-                                <h5 className="modal-title" id="exampleModalScrollableTitle">靈感來自章魚腳 清交大團隊研發助腎臟病患者掌握黃金治療時間</h5>
-                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div className="modal-body">
-                            <div className="col-md-4" >
-                                <img src={news01}
-                                    className="card-img img-news"
-                                    alt="..." />
-                            </div>
-                            <br/>
 
-                            台灣學界開發出多項全球首創技術，創新醫材造福腎臟病患者，清大教授洪健中團隊取自章魚腳靈感，研發出1滴血、10分鐘完成檢測的血液過濾器，有助患者提早48小時掌握治療時間；交大教授冉曉雯團隊則開發出呼氣就能馬上快篩腎臟病高危險群和洗腎成效的醫材。
-                            <br/>
-                            <br/>
-                            科技部從2017年5月起推動為期4年的創新醫療器材專案計畫，科技部表示，計畫透過政府和產學研合作體系，導引醫療器材研發的跨領與整合，至今專利申請達56件、核准32件；技術移轉11件，共成立10家新創公司，促進廠商投資9.9億元。
-                            </div>
-                            <div className="modal-footer">
-                                <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </section>
         </div>
         );
--- src/routes/Research.js
+++ src/routes/Research.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'dva';
+import data from '../data/research'
 
 class Research extends Component {
   renderReseach = (res) => {
-    return <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-      <div className="resume-content">
-        <h3 className="mb-0">{res.title}</h3>
-        <div className="subheading mb-1">{res.subtitle}</div>
-        <p>{res.content1 ? res.content1 : ''}</p>
-        <p>{res.content2 ? res.content2 : ''}</p>
-        {res.reference ? <a href={res.reference}>Reference</a> : null}
-      </div>
-      <div className="resume-date text-md-right">
-        <span className="text-primary">{res.period}</span>
+    return <div class="col mb-4">
+      <div class="card h-100">
+        <div class="card-body">
+          <h5 class="card-title">{res.research}</h5>
+          <p class="card-text">{res.discribe}</p>
+        </div>
       </div>
     </div>
   }
@@ -25,45 +22,18 @@ class Research extends Component {
             <div className="container">
               <div className="row">
                 <div className="col-lg-12 text-center">
-                  <h3 className="section-subheading text-muted">BioMEMS & NanoBio System</h3>
+                  <h3 className="section-subheading text-muted">{data.title}</h3>
                   <hr className="objective_divider my-4"></hr>
                   <h5 className="section-heading">
-                  本實驗室主要是進行生醫感測器(血液檢測與呼氣檢測)、食品安全檢測晶片、奈米線、仿生電子抗體、次世代DNA定序研發與系統整合,
-                  以及相關所衍生奈米科技、生醫 電子、智慧型高分子之基礎科學現象探討
+                    {data.content}
                   </h5>
                 </div>
               </div>
             </div>
           </section>
           <div class="row row-cols-1 row-cols-md-3">
-            <div class="col mb-4">
-              <div class="card h-100">
-                <div class="card-body">
-                  <h5 class="card-title">生醫感測器及微系統整合</h5>
-                  <p class="card-text">生醫電子感測器、光電奈米線感測器、 壓電聲波微發送器、
-                    非侵入式呼氣疾病檢測系統、 多通道生醫感測系統、Android人機介面、
-                    定點照護(point-of-care)之可攜式(portable)或手持式(handheld)嵌入式感測平台</p>
-                </div>
-              </div>
-            </div>
-            <div class="col mb-4">
-              <div class="card h-100">
-                <div class="card-body">
-                  <h5 class="card-title">仿生高分子技術與微流體技術</h5>
-                  <p class="card-text">仿生真空吸引模組、仿生分子拓印電子抗體、微流體液滴操控、
-                    乳化液滴高分子製程、多通道微流體操控 、高效能全血血漿分離元件</p>
-                </div>
-              </div>
-            </div>
-            <div class="col mb-4">
-              <div class="card h-100">
-                <div class="card-body">
-                  <h5 class="card-title">奈米科技與生醫奈米</h5>
-                  <p class="card-text">噴印與滾印製程、尖端奈米元件與製程、奈米生物電子元件、混成奈米線、
-                    DNA生物電子定序元件、混成高分子奈米發電元件</p>
-                </div>
-              </div>
-            </div>
+            {data.list.map(res => this.renderReseach(res))}
+
           </div>
         </div>
 
