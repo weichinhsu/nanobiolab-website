d116d058b59558f62590736a813244bcf6ce08d6
Winnie Hsu <wchinhsu@gmail.com>
2020-03-06 17:30:52 +0800

    Updates

--- js/animation.js
+++ js/animation.js
@@ -2,6 +2,10 @@ $(document).ready(function () {
     var nthu = document.getElementById('nthu');
     var nthu_show = document.getElementById('wel-show');
     var nthu_enter = document.getElementById('wel-enter');
+    $('<img/>').attr('src', '../img/welcome.jpg').on('load', function () {
+        $(this).remove(); // prevent memory leaks as @benweet suggested
+        nthu_show.style["display"] = 'block'
+    });
     nthu.addEventListener('animationend', function () {
         console.log('動畫開始');
         nthu_show.style["display"] = 'none'
@@ -11,5 +15,5 @@ $(document).ready(function () {
         // nthu_text_span.style["animation"] = 'slidein 1s'
         // nthu_text_span.style.animationFillMode="forwards"
     })
-    
+
 })
